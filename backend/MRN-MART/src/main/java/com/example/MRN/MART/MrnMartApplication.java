de60b001b26dec576503147cb12d2aa06babbef1
rohith26 <guttarohith95023@gmail.com>
2025-12-01 13:07:08 +0530

    backend structure updated

---
+++ backend/MRN-MART/src/main/java/com/example/MRN/MART/MrnMartApplication.java
@@ -0,0 +1,13 @@
+package com.example.MRN.MART;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrnMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrnMartApplication.class, args);
+	}
+
+}
